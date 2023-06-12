4cb04adc33bc8e76e79a94f7b928856381623fe1
daimessdn@gmail.com <daimessdn@gmail.com>
2023-06-12 08:41:17 +0700

    fixed routing bug with adding component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+
 const routes: Routes = [
-  { path: "listings", pathMatch: "full" }
+  { path: "listings", component: ListingsPageComponent, pathMatch: "full" }
 ];
 
 @NgModule({
